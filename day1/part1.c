4e489308fd134f25ddd756bfe2170e730b1b77c2
nikita <nikita20021804@gmail.com>
2024-10-12 21:07:24 +0300

    Solved part 1 of Day 1.

--- day1/part1.c
+++ day1/part1.c
@@ -1,3 +1,43 @@
+#include <ctype.h>
 #include <stdio.h>
+#include <stdlib.h>
+#include <string.h>
 
-int main() { printf("test"); }
+#define MAX_LINE_LENGTH 1000
+
+int main() {
+    FILE *file;
+    char line[MAX_LINE_LENGTH];
+    int sum = 0;
+
+    // Open the file
+    file = fopen("input", "r");
+    if (file == NULL) {
+        printf("Error opening the file\n");
+        return 1;
+    }
+
+    // Read and print each line
+    while (fgets(line, sizeof(line), file) != NULL) {
+        char digits[MAX_LINE_LENGTH];
+        int digit_index = 0;
+        for (int i = 0; i < strlen(line) - 1; i++) {
+            if (isdigit(line[i]) != 0) {
+                digits[digit_index] = line[i];
+                digit_index++;
+            }
+        }
+
+        char num_to_sum[3] = {digits[0], digits[digit_index - 1], '\0'};
+        int num = atoi(num_to_sum);
+
+        sum += num;
+    }
+
+    // Close the file
+    fclose(file);
+
+    printf("Sum of all of the calibration values: %d", sum);
+
+    return 0;
+}
